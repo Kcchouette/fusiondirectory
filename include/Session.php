c411be6331b8ab392a1b69b786c5012cb3819b2f
Kcchouette <kcchouette@posteo.org>
2026-06-14 11:12:02 +0200

    security: harden session configuration
    - Add session.cookie_samesite=Lax
    - Add session.use_strict_mode=1

--- include/Session.php
+++ include/Session.php
@@ -158,6 +158,8 @@ class Session
      *  allowing possible XSS attacks
      */
     ini_set("session.cookie_httponly", "1");
+    ini_set("session.cookie_samesite", "Lax");
+    ini_set("session.use_strict_mode", "1");
 
     if ($id !== NULL) {
       session_id($id);
